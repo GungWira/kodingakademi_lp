148b06ef2ecdb8b8634f2bc167df68152d9b423e
GungWira <wiradarma002@gmail.com>
2025-08-05 15:41:54 +0800

    Finish junior program responsive

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -63,21 +63,21 @@ export default function Homepage() {
             <img
               src="/homepage-bootcamp.webp"
               alt="bootcamp image koding akademi"
-              className="h-full object-cover relative z-0 rounded-2xl"
+              className="h-full object-cover relative z-0 rounded-lg sm:rounded-2xl"
             />
             <img
               src="/homepage-bootcamp-vec.webp"
               alt="bootcamp vector koding akademi"
-              className="absolute h-24 sm:h-32 -top-12 -right-8"
+              className="absolute h-20 sm:h-32 -top-12 -right-4"
             />
-            <div className="bg-white p-5 rounded-md flex items-center gap-4 absolute bottom-6 left-6">
+            <div className="bg-white p-3 sm:p-5 rounded-md flex items-center gap-3 sm:gap-4 absolute sm:bottom-6 sm:left-6 bottom-3 left-3 max-w-[90%]">
               <img
                 src="/homepage-bootcamp-icon.webp"
                 alt="bootcamp icon"
-                className="h-9 sm:h-10"
+                className="h-8 sm:h-10"
               />
               <div className="flex flex-col justify-start items-start">
-                <p className="text-base sm:text-lg font-semibold">
+                <p className="text-sm sm:text-base md:text-lg font-semibold">
                   Certified Mentor
                 </p>
                 <p className="text-xs sm:text-sm opacity-70">
@@ -158,14 +158,14 @@ export default function Homepage() {
                 alt="senior program image koding akademi"
                 className="w-full h-full object-cover"
               />
-              <div className="bg-white p-5 rounded-md flex items-center gap-3 sm:gap-4 absolute bottom-6 left-6">
+              <div className="bg-white p-3 sm:p-5 rounded-md flex items-center gap-3 sm:gap-4 absolute sm:bottom-6 sm:left-6 bottom-3 left-3 max-w-[90%]">
                 <img
-                  src="/homepage-senior-program-icon-1.webp"
-                  alt="bootcamp icon"
+                  src="homepage-senior-program-icon-1.webp"
+                  alt="senior program icon"
                   className="h-8 sm:h-10"
                 />
                 <div className="flex flex-col justify-start items-start">
-                  <p className="text-base sm:text-lg font-semibold">
+                  <p className="text-sm sm:text-base md:text-lg font-semibold">
                     Competitive Student
                   </p>
                   <p className="text-xs sm:text-sm opacity-70">
@@ -182,14 +182,14 @@ export default function Homepage() {
                 alt="senior program image koding akademi"
                 className="w-full h-full object-cover"
               />
-              <div className="bg-white p-5 rounded-md flex items-center gap-3 sm:gap-4 absolute bottom-6 left-6">
+              <div className="bg-white p-3 sm:p-5 rounded-md flex items-center gap-3 sm:gap-4 absolute sm:bottom-6 sm:left-6 bottom-3 left-3 max-w-[90%]">
                 <img
-                  src="/homepage-senior-program-icon-2.webp"
-                  alt="bootcamp icon"
+                  src="homepage-senior-program-icon-2.webp"
+                  alt="senior program icon"
                   className="h-8 sm:h-10"
                 />
                 <div className="flex flex-col justify-start items-start">
-                  <p className="text-base sm:text-lg font-semibold">
+                  <p className="text-sm sm:text-base md:text-lg font-semibold">
                     World Curriculum
                   </p>
                   <p className="text-xs sm:text-sm opacity-70">
@@ -200,7 +200,7 @@ export default function Homepage() {
             </div>
             <div className="flex justify-between items-start gap-8 w-full">
               <div className="flex flex-col gap-3 flex-1">
-                <p className="text-4xl sm:text-5xl font-bold">92%</p>
+                <p className="text-3xl sm:text-5xl font-bold">92%</p>
                 <p className="text-sm sm:text-base opacity-80">
                   Ratensi Pengguna
                 </p>
@@ -209,7 +209,7 @@ export default function Homepage() {
                 </div>
               </div>
               <div className="flex flex-col gap-3 flex-1">
-                <p className="text-4xl sm:text-5xl font-bold">97%</p>
+                <p className="text-3xl sm:text-5xl font-bold">97%</p>
                 <p className="text-sm sm:text-base opacity-80">Pengguna Puas</p>
                 <div className="w-full h-1 rounded-2xl bg-[#E8E8E8] overflow-hidden">
                   <div className="w-[97%] h-full bg-[#006EFF] rounded-2xl"></div>
@@ -221,7 +221,7 @@ export default function Homepage() {
       </section>
       {/* END SENIOR PROGRAM */}
       {/* START JUNIOR PROGRAM */}
-      {/* <section className="w-full flex justify-center items-center relative">
+      <section className="w-full flex justify-center items-center relative">
         <div className="flex relative overflow-hidden flex-col justify-center items-center w-full">
           <div className="flex justify-center relative -bottom-1 items-center w-full overflow-hidden">
             <img
@@ -230,66 +230,67 @@ export default function Homepage() {
               className="min-w-7xl"
             />
           </div>
-          <div className="bg-[#006EFF] flex flex-col justify-center py-8 items-center gap-4 w-full relative overflow-hidden">
-            <div className="flex w-full max-w-6xl text-center relative justify-center items-center gap-8">
+          <div className="bg-[#006EFF] flex flex-col justify-center py-8 pb-6 items-center gap-4 w-full relative overflow-hidden">
+            <div className="flex w-full max-w-6xl text-center relative justify-center items-center gap-8 mt-8 sm:my-0">
               <img
                 src="/junior-program-cloud-1.webp"
                 alt="clound img koding akademi"
-                className="min-w-52 w-52"
+                className="sm:min-w-52 sm:w-52 w-40 min-w-40 absolute md:relative -top-14 sm:top-0 -left-1/6 md:left-0"
               />
               <div className="flex flex-col justify-center items-center gap-4">
-                <span className="subtitle text-[#FFFFFF] font-medium text-base uppercase tracking-widest">
-                  Junior Program
-                </span>
-                <h2 className="text-4xl font-bold leading-12 text-[#FFFFFF]">
-                  Accelerated Learning for Future Innovators
-                </h2>
+                <TitleLayout
+                  title="Let’s Start Learning Today"
+                  subtitle="Junior Program"
+                  align="Center"
+                  position="Center"
+                  type="White"
+                />
               </div>
               <img
                 src="/junior-program-cloud-2.webp"
                 alt="clound img koding akademi"
-                className="min-w-52 w-52"
+                className="sm:min-w-52 sm:w-52 w-40 min-w-40 absolute md:relative -top-16 sm:top-0 -right-1/6 md:right-0"
               />
             </div>
           </div>
           <div className="bg-[#006EFF] flex flex-col justify-center items-center w-full relative">
-            <div className="w-full flex flex-col pt-12 justify-center items-center relative max-w-6xl z-40">
+            <div className="w-full flex flex-col pt-4 sm:pt-12 justify-center items-center relative max-w-6xl z-50 px-6 sm:px-8">
               <div className="w-full max-w-4xl aspect-21/11 bg-white rounded-2xl relative">
                 <img
                   src="/junior-program-character-1.webp"
                   alt="character koding akademi"
-                  className="absolute -top-16 -left-28 h-40"
+                  className="absolute -top-8 sm:-top-16 -left-12 md:-left-28 md:h-40 sm:h-32 h-20"
                 />
                 <img
                   src="/junior-program-character-2.webp"
                   alt="character koding akademi"
-                  className="absolute -top-16 -right-20 h-36"
+                  className="absolute -top-8 sm:-top-16 -right-12 md:-right-20 md:h-36 sm:h-28 h-18"
                 />
               </div>
-              <div className="w-full max-w-2xl bg-white relative -top-12 rounded-3xl p-6 shadow-xl flex flex-col justify-center items-center gap-8 inner-shadow-combined-2">
-                <div className="bg-[#006EFF] px-12 tracking-widest inner-shadow-combined py-3 text-white font-medium rounded-full">
+              <div className="w-full  sm:max-w-fit md:max-w-2xl bg-white relative z-[60] top-0 mt-8 sm:mt-0 sm:-top-12 rounded-3xl p-4 sm:p-6 shadow-xl flex flex-col justify-center items-center sm:gap-8 gap-5 inner-shadow-combined-2">
+                <div className="bg-[#006EFF] absolute sm:relative -top-3 sm:top-0 text-xs sm:text-sm md:text-base px-9 sm:px-12 tracking-widest inner-shadow-combined py-2 sm:py-3 text-white font-medium rounded-full">
                   PLATFORM
                 </div>
-                <div className="flex flex-row w-full justify-center items-center gap-8">
+                <div className="flex mt-6 sm:mt-0 flex-row w-full justify-center items-center sm:gap-8 gap-4 flex-wrap">
                   <img
                     src="/scartch-logo.webp"
                     alt="scratch logo"
-                    className="h-9"
+                    className="h-7 sm:h-9"
                   />
                   <img
                     src="/code-org-logo.webp"
                     alt="scratch logo"
-                    className="h-9"
+                    className="h-7 sm:h-9"
                   />
                   <img
                     src="/construct-3-logo.webp"
                     alt="scratch logo"
-                    className="h-9"
+                    className="h-7 sm:h-9"
                   />
                   <img
                     src="/roblox-studio-logo.webp"
                     alt="scratch logo"
-                    className="h-9"
+                    className="h-7 sm:h-9"
                   />
                 </div>
               </div>
@@ -298,27 +299,27 @@ export default function Homepage() {
               <img
                 src="/dino-1.webp"
                 alt="dinosaurus image koding akademi"
-                className="absolute bottom-0 h-64 -left-8"
+                className="absolute bottom-0 h-36 sm:h-52 md:h-64 -left-8"
               />
               <img
                 src="/dino-2.webp"
                 alt="dinosaurus image koding akademi"
-                className="absolute bottom-0 h-64 -right-8"
+                className="absolute bottom-0 h-40 sm:h-52 md:h-64 -right-8"
               />
             </div>
             <img
               src="/junior-program-bottom-1.webp"
               alt="background custom koding akademi"
-              className="absolute bottom-0 right-0 min-w-7xl w-7xl z-10"
+              className="absolute bottom-0 right-0 max-h-[60%] sm:max-h-full min-w-7xl w-7xl z-10"
             />
             <img
               src="/junior-program-bottom-2.webp"
               alt="background custom koding akademi"
-              className="absolute bottom-0 left-0 min-w-7xl w-7xl z-0"
+              className="absolute bottom-0 left-0 max-h-[60%] sm:max-h-full min-w-7xl w-7xl z-0"
             />
           </div>
         </div>
-      </section> */}
+      </section>
       {/* END JUNIOR PROGRAM */}
       {/* START WHY CODING */}
       {/* <section className="flex justify-center items-center w-full">
